5433ad292b8cd308c40e119971ef48d1e1b27dc3
VictorG10 <victorgbadebo10@gmail.com>
2025-11-24 08:57:56 +0100

    initial commit

---
+++ src/components/layouts/Layout.vue
@@ -0,0 +1,71 @@
+<script setup>
+  import img from "/victor.jpg";
+</script>
+
+<template>
+  <header>
+    <h1 class="text-gradient">FitnessVilla</h1>
+  </header>
+
+  <main>
+    <slot></slot>
+  </main>
+
+  <footer>
+    <small>Created by </small>
+    <a href="https://www.github.com/VictorG10" target="_blank">
+      <img :src="img" alt="pfp" />
+      <p>VictorG10</p>
+
+      <i class="fa-brands fa-github"></i>
+    </a>
+  </footer>
+</template>
+
+<style scoped>
+  header,
+  main,
+  footer {
+    padding: 1rem;
+    width: 100%;
+    max-width: 700px;
+    margin: 0 auto;
+  }
+
+  main {
+    flex: 1;
+  }
+
+  footer {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    align-items: center;
+    /* justify-content: center; */
+    padding: 2rem 0;
+    padding-bottom: 3rem;
+  }
+
+  footer a {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    padding: 0.3rem;
+    padding-right: 0.5rem;
+    background-color: var(--background-muted);
+    border-radius: 4rem;
+    border: 1px solid transparent;
+    transition-duration: 200ms;
+    text-decoration: none;
+  }
+
+  footer a:hover {
+    border-color: var(--color-link);
+  }
+
+  footer a img {
+    max-width: 30px;
+    aspect-ratio: 1/1;
+    border-radius: 100%;
+  }
+</style>
---
+++ src/components/pages/Workout.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>Workout</div>
+</template>
+
+<style scoped></style>
